c530f4d3b9ca5c63340782b3729429aef1a36b5f
Claudia <40446421+thepenskefile@users.noreply.github.com>
2025-04-14 20:28:59 +1000

    feature: add kudos send address and message (#5)

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./components/button";
 export * from "./components/table";
+export * from "./components/page-content";
+export * from "./components/input";
+export * from "./components/textarea";
 export { cn } from "./utils/cn";
