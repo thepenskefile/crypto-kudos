19cb0d85f8b86f1711235087cd95fd1f2a5d44ae
Claudia Nadalin <40446421+thepenskefile@users.noreply.github.com>
2025-04-14 18:21:24 +1000

    wip

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -3,16 +3,6 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      mining: {
-        auto: true,
-        interval: 0,
-      },
-      chainId: 31337,
-    },
-  },
 };
 
 export default config;
