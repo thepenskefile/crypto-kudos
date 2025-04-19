70cd40b815fc289ec49e7eaad8fbed43b6bd8ceb
Claudia Nadalin <40446421+thepenskefile@users.noreply.github.com>
2025-04-19 16:40:04 +1000

    feature: deploy to mainnet

---
+++ packages/shared/src/deployments.1.ts
@@ -0,0 +1,260 @@
+// AUTO-GENERATED ABI FILE
+export const deployment1 = {
+  "address": "0x1b92748d0495e2fe2b20c3e8f5798accd3b373f0",
+  "abi": [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "message",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "bytes32",
+          "name": "transactionHash",
+          "type": "bytes32"
+        }
+      ],
+      "name": "KudoSent",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "components": [
+            {
+              "internalType": "uint256",
+              "name": "page",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "pageSize",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct Kudos.GetKudosParams",
+          "name": "params",
+          "type": "tuple"
+        }
+      ],
+      "name": "getKudosReceived",
+      "outputs": [
+        {
+          "components": [
+            {
+              "components": [
+                {
+                  "internalType": "address",
+                  "name": "from",
+                  "type": "address"
+                },
+                {
+                  "internalType": "address",
+                  "name": "to",
+                  "type": "address"
+                },
+                {
+                  "internalType": "string",
+                  "name": "message",
+                  "type": "string"
+                },
+                {
+                  "internalType": "uint256",
+                  "name": "timestamp",
+                  "type": "uint256"
+                },
+                {
+                  "internalType": "bytes32",
+                  "name": "transactionHash",
+                  "type": "bytes32"
+                }
+              ],
+              "internalType": "struct Kudos.Kudo[]",
+              "name": "kudos",
+              "type": "tuple[]"
+            },
+            {
+              "internalType": "uint256",
+              "name": "currentPage",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "pageSize",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "totalPages",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "totalItems",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct Kudos.PaginatedKudos",
+          "name": "",
+          "type": "tuple"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getKudosReceivedCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "components": [
+            {
+              "internalType": "uint256",
+              "name": "page",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "pageSize",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct Kudos.GetKudosParams",
+          "name": "params",
+          "type": "tuple"
+        }
+      ],
+      "name": "getKudosSent",
+      "outputs": [
+        {
+          "components": [
+            {
+              "components": [
+                {
+                  "internalType": "address",
+                  "name": "from",
+                  "type": "address"
+                },
+                {
+                  "internalType": "address",
+                  "name": "to",
+                  "type": "address"
+                },
+                {
+                  "internalType": "string",
+                  "name": "message",
+                  "type": "string"
+                },
+                {
+                  "internalType": "uint256",
+                  "name": "timestamp",
+                  "type": "uint256"
+                },
+                {
+                  "internalType": "bytes32",
+                  "name": "transactionHash",
+                  "type": "bytes32"
+                }
+              ],
+              "internalType": "struct Kudos.Kudo[]",
+              "name": "kudos",
+              "type": "tuple[]"
+            },
+            {
+              "internalType": "uint256",
+              "name": "currentPage",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "pageSize",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "totalPages",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "totalItems",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct Kudos.PaginatedKudos",
+          "name": "",
+          "type": "tuple"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getKudosSentCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "components": [
+            {
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "internalType": "string",
+              "name": "message",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct Kudos.SendKudoParams",
+          "name": "params",
+          "type": "tuple"
+        }
+      ],
+      "name": "sendKudo",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ],
+  "network": "mainnet",
+  "chainId": 1
+} as const;
