61d11c3d28bd66353ebfc3f682ec3d864d6a7ff9
Claudia <40446421+thepenskefile@users.noreply.github.com>
2025-04-19 16:40:33 +1000

    feature: deploy to mainnet (#24)

--- packages/shared/src/getDeployment.ts
+++ packages/shared/src/getDeployment.ts
@@ -1,16 +1,20 @@
 import { deployment31337 } from "./deployments.31337";
 import { deployment11155111 } from "./deployments.11155111";
 import { deployment8453 } from "./deployments.8453";
-type ChainId = number; // Add more chain IDs as needed
+import { deployment1 } from "./deployments.1";
+
+type ChainId = number;
 type Deployment =
   | typeof deployment31337
   | typeof deployment11155111
-  | typeof deployment8453;
+  | typeof deployment8453
+  | typeof deployment1;
 
 const deployments: Record<ChainId, Deployment> = {
   31337: deployment31337,
   11155111: deployment11155111,
   8453: deployment8453,
+  1: deployment1,
 } as const;
 
 export function getDeploymentByChainId(chainId: ChainId) {
